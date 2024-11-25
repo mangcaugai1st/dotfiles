909145269729f892241445569566269b916b7061
mangcaugai1st <mangcaugai1st@gmail.com>
2024-11-25 22:38:30 +0700

    Add some LSP

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,9 +1,12 @@
--- require 'lspconfig'..setup{}
+-- require 'lspconfig'.setup{}
 
+-- pyright is a lsp for Python
 require 'lspconfig'.pyright.setup{}
 
+-- clangd is a lsp for C/C++
 require 'lspconfig'.clangd.setup{}
 
+-- gopls is a lsp for Golang
 require 'lspconfig'.gopls.setup({
   settings = {
     gopls = {
@@ -16,6 +19,7 @@ require 'lspconfig'.gopls.setup({
   },
 })
 
+-- omnisharp is a lsp for C#, .Net
 require 'lspconfig'.omnisharp.setup {
   on_attach = on_attach,
   handlers = handlers,
@@ -23,5 +27,23 @@ require 'lspconfig'.omnisharp.setup {
   cmd = { "dotnet", "C:\\Users\\mangc\\AppData\\Local\\nvim-data\\mason\\packages\\omnisharp\\libexec\\OmniSharp.dll"},
 }
 
+-- emmet-ls is a lsp for html, css, javascript, typescript, sass, less
+require 'lspconfig'.emmet_ls.setup { 
+  cmd = { "emmet-ls", "--stdio" }, 
+  filetypes = { "html", "css", "javascript", "typescript", "sass", "less" }, 
+}
+
 -- require'lspconfig'.csharp_ls.setup{}
+
+
+-- vscode-langservers-extracted is a lsp for html, css, json, eslint.
+-- Warning 
+-- You must install vscode-langservers-extracted via npm,
+-- Because it not available in Mason.
+-- Commnand to install it: 
+-- npm i -g vscode-langservers-extracted
+require 'lspconfig'.jsonls.setup{
+  cmd = { "vscode-json-language-server", "--stdio"},
+  filetypes = { "json" },
+}
 
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -3,7 +3,13 @@ return {
   config = function()  
     require("mason").setup()
     require("mason-lspconfig").setup{
-      ensure_installed = {"clangd", "omnisharp", "gopls", "pyright"},
+      ensure_installed = {
+        "clangd",
+        "omnisharp",
+        "gopls",
+        "pyright",
+        "emmet_ls"
+      },
       -- automatic_installation = true,
     }
   end, 
